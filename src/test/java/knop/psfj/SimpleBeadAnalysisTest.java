590620c5f1aae4b9e7d0eb5fe2f1015f397b93ee
Cyril MONGIS <cyril.mongis@googlemail.com>
2018-08-15 16:45:14 +0200

    Added test that make sure that PSFj core modification don't affect bead calculation and parameters.

--- src/test/java/knop/psfj/SimpleBeadAnalysisTest.java
+++ src/test/java/knop/psfj/SimpleBeadAnalysisTest.java
@@ -5,6 +5,7 @@
  */
 package knop.psfj;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
@@ -20,6 +21,24 @@ public class SimpleBeadAnalysisTest {
         BeadImageManager manager = new BeadImageManager();
         
         manager.add("./src/test/resources/ch1_ApoTIRF_60x.tif");
+        
+        manager.autoFocus(0);
+         manager.setThresholdValue(1533);
+        manager.setFrameSize(17);
+       
+        
+        manager.processBeadImages();
+        
+        int validBeads = manager.getBeadFrameList().getValidBeadFrameCount();
+        int invalidBeads = manager.getBeadFrameList().getNonValidBeadFrameCount();
+        FovDataSet dataset = manager.getDataSet();
+       // double xMedian = new Double(dataset.getColumnMedian(PSFj.));
+       // double yMedian = new Double(dataset.getColumnMedian(PSFj.FWHM_KEY[1]));
+       // double zMedian = new Double(dataset.getColumnMedian(PSFj.FWHM_KEY[2]));
+        
+        assertEquals("Valid beads",29,validBeads);
+        assertEquals("Invalid beads",35,invalidBeads);
+        
         
     }
     
