2f9761aa69e6731995bdb7f08a80ecca5ded9101
Cyril MONGIS <cyril.mongis@googlemail.com>
2018-08-16 06:50:17 +0200

    Added a new class called bead image loader

---
+++ src/main/java/knop/psfj/BeadImageLoader.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knop.psfj;
+
+import java.util.List;
+
+/**
+ *
+ * @author cyril
+ */
+public interface BeadImageLoader {
+    
+    
+    List<BeadImage> load(String fileAddress) throws BeadImageLoadingException;
+    
+    
+}
---
+++ src/main/java/knop/psfj/DefaultBeadImageLoader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knop.psfj;
+
+import java.util.List;
+
+/**
+ *
+ * @author cyril
+ */
+public class DefaultBeadImageLoader implements BeadImageLoader {
+
+    @Override
+    public List<BeadImage> load(String fileAddress) throws BeadImageLoadingException{
+        
+        return null;
+    }
+    
+}
